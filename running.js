664d2811bd5ec5a61b0b2dd7ce1dc65a0dc18035
Asher <asher.p.lanthier@gmail.com>
2026-01-02 13:30:22 -0500

    Add files via upload

---
+++ running.js
@@ -0,0 +1,19 @@
+var full_date = new Date();
+var today = full_date.toLocaleDateString('en-US', {
+    month: '2-digit',
+    day: '2-digit',
+    year: 'numeric'
+})
+var date_display = document.getElementById('date_display');
+date_display.textContent = today;
+
+function addData(){
+    const URL = 'https://script.google.com/macros/s/AKfycbziytNllA4c_8J2BqD61LfVdkDSdnAXyV8VepliCVWkEDPiDuT0GJFTUL1J2uKbnHey6A/exec';
+    var miles = document.getElementById('miles_input').value;
+
+    let formData = new FormData();
+    formData.append('Date', today);
+    formData.append('Miles', miles);
+
+    fetch(URL, {method:'POST', body: formData});
+}
