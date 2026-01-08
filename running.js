3982689c205605c8226efce4aad2ed00b34da8ed
Asher <asher.p.lanthier@gmail.com>
2026-01-08 15:48:25 -0500

    I changed the url

--- running.js
+++ running.js
@@ -8,7 +8,7 @@ var date_display = document.getElementById('date_display');
 date_display.textContent = today;
 
 function addData(){
-    const URL = 'https://script.google.com/macros/s/AKfycbwoRA39pXcqevzp6VQKiMICehIm_xmK3RF7pKEWjdqk0WCxZZXU17T5N1wiyLwioqcd5Q/exec';
+    const URL = 'https://script.google.com/macros/s/AKfycbywh-ynHPnRL3lJ765ZyWwzOJL2Ap1Jjqo6NffM7uQWt2HS6JpmaGz2hb5N5dma-wbw/exec';
     var miles = document.getElementById('miles_input').value;
 
     let formData = new FormData();
